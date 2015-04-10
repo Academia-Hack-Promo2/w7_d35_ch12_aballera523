34c570afe965988a1d314d7b9868df0f307446e5
Alex Ballera <alexballera@gmail.com>
2015-04-10 01:31:21 -0430

    Consumiendo API

--- consumiendoapi.rb
+++ consumiendoapi.rb
@@ -7,14 +7,18 @@ class Mashable
 	base_uri 'http://mashable.com/stories.json'
 
 	def notices
+		feed_mashable = Array.new
 		response = HTTParty.get('http://mashable.com/stories.json')
 		response["new"].each do |notice|
-			puts notice['author']
-			puts notice['title']
-			puts notice['post_date']
-			puts notice['link']
-			puts "-"*64
+			author = notice['author']
+			title = notice['title']
+			date = notice['post_date']
+			url = notice['link']
+			website = 'Mashable'
+			notice = Notice.new(author, title, date, url, website)
+			feed_mashable.push(notice)
 		end
+		return feed_mashable
 	end
 end
 
@@ -23,14 +27,18 @@ class Digg
 	base_uri 'http://digg.com/api/news/popular.json'
 
 	def notices
+		feed_digg = Array.new
 		response = HTTParty.get('http://digg.com/api/news/popular.json')
 		response["data"]["feed"].each do |notice|
-			puts notice["content"]['author']
-			puts notice["content"]['title']
-			puts notice["content"]['date_published']
-			puts notice["content"]['url']
-			puts "-"*64
+			author = notice["content"]['author']
+			title = notice["content"]['title']
+			date = notice["content"]['date_published']
+			url = notice["content"]['url']
+			website = 'Digg'
+			notice = Notice.new(author, title, date, url, website)
+			feed_digg.push(notice)
 		end
+		return feed_digg
 	end
 end
 
@@ -39,32 +47,86 @@ class Reddit
 	base_uri 'http://www.reddit.com/.json'
 
 	def notices
+		feed_reddit = Array.new
 		response = HTTParty.get('http://www.reddit.com/.json')
 		response["data"]["children"].each do |notice|
-			puts notice["data"]['author']
-			puts notice["data"]['title']
-			puts notice["data"]['created']
-			puts notice["data"]['url']
-			puts "-"*64
+			author = notice["data"]['author']
+			title = notice["data"]['title']
+			date = notice["data"]['created']
+			url = notice["data"]['url']
+			website = 'Reddit'
+			notice = Notice.new(author, title, date, url, website)
+			feed_reddit.push(notice)
 		end
+		return feed_reddit
+	end
+end
+
+class Feed
+	def response website
+		
+		if website == 'Mashable'
+			m = Mashable.new
+			blog = m.notices
+			return blog
+		elsif website == 'Reddit'
+			r = Reddit.new
+			blog = r.notices
+			return blog
+		elsif website == 'Digg'
+			d = Digg.new
+			blog = d.notices
+			return blog							
+		end
+	end	
+end
+
+class Notice
+	attr_accessor :author, :title, :date, :url, :website
+
+	def initialize author, title, date, url, website
+		@author = author
+		@title = title
+		@date = date
+		@url = url
+		@website = website	
 	end
 end
 
 def main
-	puts "*"*64
-	puts 'Mashable'
-	puts "*"*64
-	news_mashable = Mashable.new
-	news_mashable.notices
-	puts "*"*64
-	puts 'Digg'
-	puts "*"*64
-	news_digg = Digg.new
-	news_digg.notices
-	puts "*"*64
-	puts 'Reddit'
-	puts "*"*64
-	news_reddit = Reddit.new
-	news_reddit.notices
+	while true
+		puts 'Indique sólo el número "1", "2" y "3" ¿Qué website desea ver?'
+		puts "'1' para Mashable"
+		puts "'2' para Reddit"
+		puts "'3' para Digg"
+		opt = gets.to_i
+
+		if opt != 1 && opt != 2 && opt != 3
+			puts 'opción inválida'
+		end
+
+		if opt == 1
+			m = Feed.new
+			opt = m.response("Mashable")
+			break
+		elsif opt == 2
+			r = Feed.new
+			opt = r.response("Reddit")
+			break
+		else
+			d = Feed.new
+			opt = d.response("Digg")
+			break
+		end
+	end
+
+	for i in 0...opt.length
+		puts "*"*64
+		puts opt[i].author
+		puts opt[i].title
+		puts opt[i].date
+		puts opt[i].url
+		puts opt[i].website
+	end
 end
 main
